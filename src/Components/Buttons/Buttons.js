6c18d2e22e091806957ea95696b2809c2a6872d5
Zoms <martinezoms15@gmail.com>
2020-12-22 02:40:00 +0100

    fixed responsiveness

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -3,28 +3,28 @@ import './buttons.css';
 export default function buttons() {
   return (
     <div className="flex flex-wrap">
-      <button className="outline w-20 pa2 br5 ">C</button>
-      <button className="outline w-20 pa2 br5 ">(</button>
-      <button className="outline w-20 pa2 br5 ">)</button>
-      <button className="outline w-20 pa2 br5 ">X</button>
-      <button className="outline w-20 pa2 br5 ">7</button>
-      <button className="outline w-20 pa2 br5 ">8</button>
-      <button className="outline w-20 pa2 br5 ">9</button>
-      <button className="outline w-20 pa2 br5 ">/</button>
-      <button className="outline w-20 pa2 br5 ">4</button>
-      <button className="outline w-20 pa2 br5 ">5</button>
-      <button className="outline w-20 pa2 br5 ">6</button>
-      <button className="outline w-20 pa2 br5 ">*</button>
-      <button className="outline w-20 pa2 br5 ">1</button>
-      <button className="outline w-20 pa2 br5 ">2</button>
-      <button className="outline w-20 pa2 br5 ">3</button>
-      <button className="outline w-20 pa2 br5 "> - </button>
-      <button className="outline w-20 pa2 br5 ">0</button>
-      <button className="outline w-20 pa2 br5 ">.</button>
-      <button className="outline w-20 pa2 br5 ">%</button>
-      <button className="outline w-20 pa2 br5 ">+</button>
-      <button className="outline w-20 pa2 br5 ">sqrt</button>
-      <button className="outline w-20 pa2 br5 ">^</button>
+      <button className="outline w-25 pa2 br5 ">C</button>
+      <button className="outline w-25 pa2 br5 ">(</button>
+      <button className="outline w-25 pa2 br5 ">)</button>
+      <button className="outline w-25 pa2 br5 ">X</button>
+      <button className="outline w-25 pa2 br5 ">7</button>
+      <button className="outline w-25 pa2 br5 ">8</button>
+      <button className="outline w-25 pa2 br5 ">9</button>
+      <button className="outline w-25 pa2 br5 ">/</button>
+      <button className="outline w-25 pa2 br5 ">4</button>
+      <button className="outline w-25 pa2 br5 ">5</button>
+      <button className="outline w-25 pa2 br5 ">6</button>
+      <button className="outline w-25 pa2 br5 ">*</button>
+      <button className="outline w-25 pa2 br5 ">1</button>
+      <button className="outline w-25 pa2 br5 ">2</button>
+      <button className="outline w-25 pa2 br5 ">3</button>
+      <button className="outline w-25 pa2 br5 "> - </button>
+      <button className="outline w-25 pa2 br5 ">0</button>
+      <button className="outline w-25 pa2 br5 ">.</button>
+      <button className="outline w-25 pa2 br5 ">%</button>
+      <button className="outline w-25 pa2 br5 ">+</button>
+      <button className="outline w-25 pa2 br5 ">sqrt</button>
+      <button className="outline w-25 pa2 br5 ">^</button>
       <button className="outline w-50 pa2 br5 equal grow">=</button>
     </div>
   );
