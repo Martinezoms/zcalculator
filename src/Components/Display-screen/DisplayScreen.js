b2c6b8d37566b77738832e862dd90ab3cee50d4f
Zoms <martinezoms15@gmail.com>
2020-12-26 00:05:43 +0100

    added float button

--- src/Components/Display-screen/DisplayScreen.js
+++ src/Components/Display-screen/DisplayScreen.js
@@ -4,12 +4,8 @@ export default function displayScreen() {
   return (
     <div>
       <form className="flex flex-column display pa2">
-        <textarea className="output-screen" rows="5" readOnly>
-          output
-        </textarea>
-        <textarea className="input-screen" rows="2">
-          input
-        </textarea>
+        <textarea className="output-screen" rows="5" readOnly></textarea>
+        <textarea className="input-screen" rows="2"></textarea>
       </form>
     </div>
   );
--- src/Components/FLoat-button/FloatButton.js
+++ src/Components/FLoat-button/FloatButton.js
@@ -1,3 +1,13 @@
+import './floatButton.css';
+
 export default function floatButton() {
-  return <div>Float button</div>;
+  return (
+    <div class="wrapper">
+      <input id="triggerButton" class="triggerButton" type="checkbox" />
+      <label for="triggerButton"></label>
+      <div class="one fa fa-facebook"></div>
+      <div class="two fa fa-twitter"></div>
+      <div class="three fa fa-instagram"></div>
+    </div>
+  );
 }
