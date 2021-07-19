ac6d0a55382f28ee35aeca65e479dd1cec52ee22
Zoms <martinezoms15@gmail.com>
2021-07-19 12:43:56 +0100

    darkmode feature is ready

--- src/Themes/themes.js
+++ src/Themes/themes.js
@@ -1,26 +1,41 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const lightMode = {
-  body: 'white',
-  color: 'black'
+  body: '#ffff',
+  color: 'black',
+  btnColor: '-internal-light-dark(rgb(239,239,239), rgb(59,59,59))',
+  btnBorderColor: '-internal-light-dark(rgb(118,118,118),rgb(133,133,133))'
 };
 
 export const darkMode = {
-  body: 'black',
-  color: 'white'
+  body: '#242424',
+  color: 'white',
+  btnColor: '#2f2f2f',
+  btnBorderColor: '#000000',
+  border: '3px solid rgb(103, 99, 99)',
+  hoverBackground: '#454444'
 };
 
 export const GlobalStyle = createGlobalStyle`
 body {
   background : ${(props) => props.theme.body};
   color : ${(props) => props.theme.color};
+  transition : all 0.4s ease;
 }
 button {
-  background : ${(props) => props.theme.body};
-  color : ${(props) => props.theme.color};
-  shadow : ${(props) => props.theme.body}
+  background : ${(props) => props.theme.btnColor};
+  border-color: ${(props) => props.theme.btnBorderColor};
+  color: ${(props) => props.theme.color}
+
+}
+button:hover {
+background :${(props) => props.theme.hoverBackground}
 }
 textarea{
   color : ${(props) => props.theme.color}
+}
+.frame {
+border: ${(props) => props.theme.border};
+
 }
 `;
