5ae0a7e13eea15ea31f254f8489af8a9431914d7
Zoms <martinezoms15@gmail.com>
2021-07-31 11:53:55 +0100

    working on the history feature

--- src/Components/History/History.js
+++ src/Components/History/History.js
@@ -1,14 +1,22 @@
 function History({ history }) {
   return (
     <div className={history === true ? 'openHistory ' : 'closeHistory '}>
-      <p className="closeButton">X</p>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
-      <p>6</p>
-      <p>7</p>
+      <div>
+        <p className="closeButton">X</p>
+      </div>
+
+      <div className="historyOutput">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>10</p>
+      </div>
     </div>
   );
 }
